09e3b742c4a19b354be767cc510bc10c7370c403
杨佳军 <651380735@qq.com>
2016-12-14 16:36:55 +0800

    no message

--- src/page/home/member/route.js
+++ src/page/home/member/route.js
@@ -24,6 +24,12 @@ let Index = Vue.extend({
         Qrcode,
         Alert,
     },
+    vuex: {
+        getters: {
+            route: (state) => state.route,
+            userInfo: (state) => state.app.user,
+        },
+    },
     ready : function(){
         this.setTitle({title:'个人'}).then((data)=>{
             console.log(data);
@@ -43,7 +49,9 @@ let Index = Vue.extend({
 
     },
     computed : {
-
+        filterAvatar(){
+            return this.userInfo.avatar !='' ? this.userInfo.avatar : 'http://img5.imgtn.bdimg.com/it/u=1457102793,1407747410&fm=23&gp=0.jpg'
+        }
     },
     route : {
         data : function(transition){
